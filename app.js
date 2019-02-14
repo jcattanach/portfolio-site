26de11318f205b4ec47bec7d0ad804655b93839c
Jack Cattanach <jackcattanach@live.com>
2019-02-13 19:51:18 -0600

    working on responsive menu

--- app.js
+++ app.js
@@ -34,3 +34,12 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
